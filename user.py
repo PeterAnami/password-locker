e6b0e3158cbb144568dd6eb5367d251e9c8851d9
peterAnami <peter.andake@student.moringaschool.com>
2021-09-05 17:24:18 +0300

    third change

--- user.py
+++ user.py
@@ -25,4 +25,28 @@ class User:
       Function to save a newly created instance
       '''
       User.users_list.append(self)
+class Credential:
+  '''
+  classs to create account credentials, generate passwords and save their information
+  '''
+  # Class variables
+  credentials_list =[]
+  user_credentials_list =[]
+  @classmethod
+  def check_user (Class,first_name,password):
+    '''
+    Method that checks if the name and password entered matches the entries in users_list
+    '''
+    current_user =''
+    for user in User.users_list:
+      if (user.first_name ==first_name and user.password == password):
+        current_user = user.first_name
+        return current_user
+  def __init__(self,user_name,site_name,account_name,password):
+    '''
+    
+
+
+
+
 
