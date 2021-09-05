4a1730f75f36e6df310f8149c234b24ce88e7bf3
peterAnami <peter.andake@student.moringaschool.com>
2021-09-05 18:22:55 +0300

    test change

--- test.py
+++ test.py
@@ -8,4 +8,16 @@ class TestUser(unittest.TestCase):
   Args:
       unittest.TestCase:helps in creating test cases
   '''
-  
+  def setUp(self):
+    '''
+    Function to create a user account before each test
+    '''
+    self.new _user = User('Peter','An\'am\'i','pswd100')
+
+  def test__init__(self):
+    '''
+    Test to if check the creation of user instances is properly done
+    '''
+    self.assertEqual(self.new_user.first_name,'Peter')
+    self.assertEqual(self.new_user.last_name,'An\'am\'i')
+    self.assertEqual(self.new_user.password,'pswd100')
