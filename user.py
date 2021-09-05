ed120e45d882d3d12dcd5c0edf26fecc54ff63b8
peterAnami <peter.andake@student.moringaschool.com>
2021-09-05 17:42:16 +0300

    changes

--- user.py
+++ user.py
@@ -54,7 +54,21 @@ class Credential:
     self.password = password
 
   def save_credentials(self)
-  
+    '''
+    Function to create a newly created user instance
+    '''
+    #global users_list
+    Credential.credentials_list.append(self)
+
+  def generate_password(size=8, char=string.ascii_uppercase+string.ascii_lowercase+string.digits):
+    '''
+    Function to generate an 8 character password for a credential
+    '''
+    gen_pass=''.join(random.choice(char)for _ in range(size))
+    return gen_pass
+
+@classmethod
+
 
 
 
