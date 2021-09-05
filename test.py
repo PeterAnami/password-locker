66b232284baf20743e00bdb34ba879a7ddb562dc
peterAnami <peter.andake@student.moringaschool.com>
2021-09-05 18:12:07 +0300

    folder test

---
+++ test.py
@@ -0,0 +1,11 @@
+import unittest
+import pyperclip
+from user import User, Credential
+
+class TestUser(unittest.TestCase):
+  '''
+  Test class that defines test cases for the user class behaviors.
+  Args:
+      unittest.TestCase:helps in creating test cases
+  '''
+  
