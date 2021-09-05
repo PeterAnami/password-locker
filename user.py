aa250c73e8d830e85dc218facfe5925b39adfb3f
peterAnami <peter.andake@student.moringaschool.com>
2021-09-05 17:02:55 +0300

    fast commit

---
+++ user.py
@@ -0,0 +1,13 @@
+import pyperclip 
+import random
+import string
+
+#Global Variables
+
+global users_list =[]
+class User:
+  '''
+  Class to create user accounts and save their information 
+  '''
+  # Class variables
+  #global users_list
