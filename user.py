ad7ac4a347fb1d3078beb475b38d02b5a8be9f02
peterAnami <peter.andake@student.moringaschool.com>
2021-09-05 17:10:49 +0300

    second commit

--- user.py
+++ user.py
@@ -4,10 +4,25 @@ import string
 
 #Global Variables
 
-global users_list =[]
+global users_list
 class User:
   '''
   Class to create user accounts and save their information 
   '''
   # Class variables
   #global users_list
+  users_list = []
+  def __init__(self, first_name, last_name,password):
+    '''
+    method to define the properties for each user objective will hold
+    '''
+    # instance variables
+    self.first_name = first_name
+    self.last_name = last_name
+    self.password = password
+    def save_user(self):
+      '''
+      Function to save a newly created instance
+      '''
+      User.users_list.append(self)
+
